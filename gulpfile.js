2f40b0d1bfea91b0c8f65c4cea1f9de339a4f27c
Rhitik <bhattrhitik95@gmail.com>
2022-10-03 16:26:46 +0530

    Adds image compression

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var gzip = require('gulp-gzip');
+const imagemin = require('gulp-imagemin');
+gulp.src('src/assets/**/*.*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('.dist/assets'))
 
 gulp.task('compress', function() {
   return gulp.src(['./dist/**/*.*'])
