a93ab7fd4f04b81cafd4a12ada0080326a8da00f
Rhitik <bhattrhitik95@gmail.com>
2022-10-03 21:20:20 +0530

    Tweaks the anchor tag generation by markdown parser, to open new tab each time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MarkdownModule } from 'ngx-markdown';
+import { MarkdownModule, MarkedOptions, MarkedRenderer } from 'ngx-markdown';
 import { AboutComponent } from './about/about.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
@@ -21,6 +21,24 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 // import { DARK_MODE_OPTIONS } from 'angular-dark-mode';
 import { PlayerComponent } from './pages/player/player.component';
 
+export function markedOptionsFactory(): MarkedOptions {
+  const renderer = new MarkedRenderer();
+  const linkRenderer = renderer.link;
+
+  renderer.link = (href, title, text) => {
+    const html = linkRenderer.call(renderer, href, title, text);
+    return html.replace(/^<a /, '<a role="link" tabindex="0" target="_blank" rel="nofollow noopener noreferrer" ');
+  };
+
+  return {
+    renderer,
+    gfm: true,
+    breaks: false,
+    pedantic: false,
+    smartLists: true,
+    smartypants: false,
+  };
+}
 
 
 @NgModule({
@@ -44,7 +62,13 @@ import { PlayerComponent } from './pages/player/player.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MarkdownModule.forRoot({ loader: HttpClient }),
+    MarkdownModule.forRoot({
+      loader: HttpClient,
+      markedOptions: {
+        provide: MarkedOptions,
+        useFactory: markedOptionsFactory
+      }
+    }),
     FormsModule,
     ReactiveFormsModule,
   ],
